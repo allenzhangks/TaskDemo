1a43fa8a1bd64c48df4c7d70208475e107667440
allenzhangks <124180690@qq.com>
2018-07-13 22:29:13 +0800

    Initial commit

---
+++ TaskDemo/CreateTaskConsole/TestRedis.cs
@@ -0,0 +1,89 @@
+﻿using DotNet.Utilities;
+using DotNet.Utilities.CommonRedis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateTaskConsole
+{
+    public static class TestRedis
+    {
+
+        /// <summary>
+        /// test消费数据存储redisDb
+        /// </summary>
+        private static long testDb = BaseBusinessLogic.ConvertToInt(ConfigurationHelper.AppSettings("redis_testDb", false));
+
+
+        /// <summary>
+        /// redis管理类
+        /// </summary>
+        private static CommonRedisClientManager _redisInstanceManager = null;
+
+
+        /// <summary>
+        /// test锁对象
+        /// </summary>
+        private static object _redisInstanceManagerLock = new object();
+
+
+        /// <summary>
+        /// reddis管理类
+        /// </summary>
+        public static CommonRedisClientManager RedisManagerInstance
+        {
+            get
+            {
+                if (_redisInstanceManager == null)
+                {
+                    lock (_redisInstanceManagerLock)
+                    {
+                        if (_redisInstanceManager == null)
+                        {
+                            _redisInstanceManager =
+                                new CommonRedisClientManager(ConfigurationHelper.AppSettings("TestRedisConn", false),
+                                    RedisClientDriverType.StackExchange);
+                        }
+                    }
+
+                }
+                return _redisInstanceManager;
+            }
+        }
+
+
+        /// <summary>
+        /// 获取Test消费数据存储redis客户端
+        /// </summary>
+        /// <returns></returns>
+        public static ICommonRedisClient TestClient
+        {
+            get
+            {
+                if (_TestClient == null)
+                {
+                    lock (_TestClientLock)
+                    {
+                        if (_TestClient == null)
+                        {
+                            _TestClient = RedisManagerInstance.GetClient();
+                            _TestClient.Db = testDb;
+                        }
+                    }
+                }
+                return _TestClient;
+            }
+        }
+        private static ICommonRedisClient _TestClient = null;
+        private static object _TestClientLock = new object();
+
+
+
+
+
+
+
+    }
+}
---
+++ TaskDemo/TaskDemo/TaskAgent.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace TaskDemo
+{
+    using DotNet.Utilities;
+    using StoXAgent;
+    public class TaskAgent : AgentServiceBase<TaskAgent>
+    {
+        public override int ThreadCount => BaseBusinessLogic.ConvertToInt(ConfigurationHelper.AppSettings("ThreadCount", false));
+
+
+        public TaskAgent()
+        {
+            ServiceName = ConfigurationHelper.AppSettings("ServiceName", false);
+            Intervals = new Int32[ThreadCount];
+            for (int i = 0; i < ThreadCount; i++)
+            {
+                if (i == 0)
+                {
+                    Intervals[i] = 60;
+                }
+                else
+                {
+                    Intervals[i] = 20;
+                }
+            }
+
+        }
+        public override void StartWork()
+        {
+            base.StartWork();
+        }
+        public override void StopWork()
+        {
+            base.StopWork();
+        }
+        public override bool Work(int index)
+        {
+            try
+            {
+                switch (index)
+                {
+                    //0 号线程 主进程是扫描任务加入到等待队列，不是主进程就处理任务。
+                    case 0:
+                        {
+                            var mainProcessKey = "MainSevice-Process";
+                            string lockKey = $"{mainProcessKey}-lock";
+                            var isMain = false;
+                            //第一次进入时，判断当前是否存在主。
+                            bool existMain = false;
+                            using (var dl = TestRedis.TestClient.AcquireLock(lockKey, new TimeSpan(0, 0, 0, 0, 1000)))
+                            {
+                                if (dl != null)
+                                {
+                                    existMain = TestRedis.TestClient.ContainsKey(mainProcessKey);
+                                    if (existMain)
+                                    {
+                                        KeyValueObject<string, DateTime> kvpMainFlag = TestRedis.TestClient.Get<KeyValueObject<string, DateTime>>(mainProcessKey);
+                                        if (kvpMainFlag.Key.Equals(ServiceName))
+                                        {
+                                            isMain = true;
+                                            TestRedis.TestClient.Set(mainProcessKey, new KeyValueObject<string, DateTime>(ServiceName, DateTime.Now));
+                                        }
+                                        else
+                                        {
+                                            if (kvpMainFlag.Value.AddMinutes(5d) < DateTime.Now)
+                                            {
+                                                isMain = true;
+                                                TestRedis.TestClient.Set(mainProcessKey, new KeyValueObject<string, DateTime>(ServiceName, DateTime.Now));
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        isMain = true;
+                                        TestRedis.TestClient.Set(mainProcessKey, new KeyValueObject<string, DateTime>(ServiceName, DateTime.Now));
+
+                                    }
+                                }
+                                else
+                                {
+                                    LogUtilities.WriteLine("获取锁失败！！！");
+                                }
+                            }
+                            TestQueueService testQueueService = new TestQueueService();
+                            if (isMain)
+                            {
+                                testQueueService.ScanQueue();
+                                LogUtilities.WriteLog("加入等待队列完毕");
+                                //ScanProcess
+                                //加入到等待队列
+                            }
+                            else
+                            {
+                                testQueueService.StartToQueueProcess(TestRedis.TestClient);
+                                LogUtilities.WriteLog("处理任务完毕");
+                                //处理任务
+                            }
+
+                            break;
+
+                        }
+                    default:
+                        {
+                            TestQueueService testQueueService = new TestQueueService();
+                            testQueueService.StartToQueueProcess(TestRedis.TestClient);
+
+                        }
+                        break;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                LogUtilities.WriteException(ex);
+            }
+
+            return base.Work(index);
+        }
+
+    }
+
+
+}
